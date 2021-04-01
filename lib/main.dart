5c861241c0b00f71faafd427a680e5b3ef715be3
mahmudalsohan <mahmud.al.sohan@gmail.com>
2021-04-01 16:20:54 +0600

    done with my way

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,13 @@ import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  var node = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,16 +24,94 @@ class XylophoneApp extends StatelessWidget {
           ),
         ),
         body: SafeArea(
-          child: Center(
-              child: ElevatedButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
-            child: Text('press'),
-          )),
+          child: Column(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    var node = 1;
+                    ringNode(node);
+                  },
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    var node = 2;
+                    ringNode(node);
+                  },
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    var node = 3;
+                    ringNode(node);
+                  },
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    var node = 4;
+                    ringNode(node);
+                  },
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    var node = 5;
+                    ringNode(node);
+                  },
+                  child: Container(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    var node = 6;
+                    ringNode(node);
+                  },
+                  child: Container(
+                    color: Colors.lightGreenAccent,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    var node = 7;
+                    ringNode(node);
+                  },
+                  child: Container(
+                    color: Colors.cyanAccent,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void ringNode(node) {
+    final player = AudioCache();
+    player.play('note$node.wav');
   }
 }
